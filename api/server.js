ebff929283fb5f8104b1b7be4b6e089fcfaaabaa
Pathompum Jirakarnpaisan <107536914+Saannddy@users.noreply.github.com>
2024-11-19 18:52:49 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,7 +1,6 @@
 const { Client } = require('@elastic/elasticsearch');
 
 // change this for your cert
-// change this for your cert
 const certificateBase64 = `
 `;
 
@@ -37,92 +36,105 @@ const port = 3000;
 
 app.use(cors({ origin: 'http://localhost:5173' }));
 
-// API endpoint to handle search requests
 app.get('/api/search', async (req, res) => {
     const { query } = req.query;
 
     if (!query || query.trim() === '') {
         return res.status(400).json({ message: 'Query string is required' });
     }
-    console.log('---------------------------------------------------------------')
+
+    console.log('---------------------------------------------------------------');
+
     try {
-        // Elasticsearch query with match query for each field
-        const result = await client.search({
-            index: 'cocktail_data_bm25',
-            body: {
-                query: {
-                    bool: {
-                        should: [
-                            {
-                                match: {
-                                    strDrink: {
-                                        query: query.toLowerCase(),
-                                        operator: 'or'
-                                    }
-                                }
-                            },
-                            {
-                                match: {
-                                    ingredients: {
-                                        query: query.toLowerCase(),
-                                        operator: 'or'
-                                    }
-                                }
-                            },
-                            {
-                                match: {
-                                    strIBA: {
-                                        query: query.toLowerCase(),
-                                        operator: 'or'
-                                    }
-                                }
-                            },
-                            {
-                                match: {
-                                    strAlcoholic: {
-                                        query: query.toLowerCase(),
-                                        operator: 'or'
-                                    }
-                                }
-                            },
-                            {
-                                match: {
-                                    strCategory: {
-                                        query: query.toLowerCase(),
-                                        operator: 'or'
-                                    }
-                                }
-                            },
-                            {
-                                match: {
-                                    strGlass: {
-                                        query: query.toLowerCase(),
-                                        operator: 'or'
-                                    }
-                                }
+        let searchQuery;
+        const alcoholicKeywords = ['alc', 'nalc', 'optalc'];
+        if (alcoholicKeywords.includes(query.toLowerCase())) {
+            searchQuery = {
+                index: 'cocktail_data_bm25',
+                body: {
+                    query: {
+                        match: {
+                            strAlcoholic: {
+                                query: query.toLowerCase(),
+                                operator: 'or'
                             }
-                        ]
+                        }
                     }
                 }
-            }
-        });
+            };
+        } else {
+            searchQuery = {
+                index: 'cocktail_data_bm25',
+                body: {
+                    query: {
+                        bool: {
+                            should: [
+                                {
+                                    match: {
+                                        strDrink: {
+                                            query: query.toLowerCase(),
+                                            fuzziness: 1,
+                                            operator: 'or'
+                                        }
+                                    }
+                                },
+                                {
+                                    match: {
+                                        ingredients: {
+                                            query: query.toLowerCase(),
+                                            fuzziness: 1,
+                                            operator: 'or'
+                                        }
+                                    }
+                                },
+                                {
+                                    match: {
+                                        strIBA: {
+                                            query: query.toLowerCase(),
+                                            fuzziness: 1,
+                                            operator: 'or'
+                                        }
+                                    }
+                                },
+                                {
+                                    match: {
+                                        strCategory: {
+                                            query: query.toLowerCase(),
+                                            fuzziness: 1,
+                                            operator: 'or'
+                                        }
+                                    }
+                                },
+                                {
+                                    match: {
+                                        strGlass: {
+                                            query: query.toLowerCase(),
+                                            fuzziness: 1,
+                                            operator: 'or'
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                }
+            };
+        }
 
-        
-        // Map the hits and transform the strAlcoholic field to custom values
+        const result = await client.search(searchQuery);
+
         const hits = result.hits.hits.map(hit => {
             const strAlcoholic = hit._source.strAlcoholic || 'N/A';
             const alcoholStatus = strAlcoholic === 'alc' ? 'Alcoholic' :
                                   strAlcoholic === 'nalc' ? 'Non-Alcoholic' :
                                   strAlcoholic === 'optalc' ? 'Optional Alcohol' :
-                                  strAlcoholic; 
-            
-            console.log(hit)
+                                  strAlcoholic;
 
             return {
                 idDrink: hit._source.idDrink || 'N/A',
                 strDrink: hit._source.strDrink || 'N/A',
                 ingredients: hit._source.ingredients || 'N/A',
-                strAlcoholic: alcoholStatus, // Use the transformed alcohol status
+                strAlcoholic: alcoholStatus, 
                 strCategory: hit._source.strCategory || 'N/A',
                 strGlass: hit._source.strGlass || 'N/A',
                 strDrinkThumb: hit._source.strDrinkThumb || 'N/A',
@@ -137,6 +149,7 @@ app.get('/api/search', async (req, res) => {
         res.status(500).json({ message: 'Error querying Elasticsearch', error: error.message });
     }
 });
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
